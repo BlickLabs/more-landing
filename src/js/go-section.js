a63b97723cf8633de97f48b5a51bae57e7d54c59
Raúl Marty <santor3medio@gmail.com>
2018-04-12 17:59:28 -0500

    Update 2018-04-12T22:59:26.888Z

--- src/js/go-section.js
+++
@@ -1,7 +0,0 @@
-var clickHandler = ('ontouchstart' in document.documentElement ? "touchstart" : "click");
-$("a.go-section").bind(clickHandler, function(e) {
-  var targetDiv = $(this).attr('href');
-  $('html, body').animate({
-      scrollTop: $(targetDiv).offset().top - 100
-  }, 800);
-});
